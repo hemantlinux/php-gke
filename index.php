f93a94a2f1c0a798f61d49721163a3266508917a
Dinesh Sharma <dineshsharmadev93@gmail.com>
2024-04-15 11:02:42 +0530

    commit

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ include("./php/config.php");
 <head>
 
     <!-- Enter a proper page title here -->
-    <title>BULKDATABASE PROVIDER Prod</title>
+    <title>BULKDATABASE PROVIDER </title>
 
     <!-- CSS to include bootstrap -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
